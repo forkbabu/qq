29ecc1c9e61b4408d911221061b148f1045ea072
Forkbabu <sayantandas30011998@gmail.com>
2020-08-09 01:45:25 +0530

    Create johnny.sh

---
+++ johnny.sh
@@ -0,0 +1,3 @@
+mkdir 'models'
+wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0 -O ./models/ColorizeArtistic_gen.pth
+wget https://media.githubusercontent.com/media/jantic/DeOldify/master/resource_images/watermark.png -O ./resource_images/watermark.png
