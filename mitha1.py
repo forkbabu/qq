14874c254ad7d7341127c853425b386359e686a9
Forkbabu <sayantandas30011998@gmail.com>
2020-08-09 01:43:56 +0530

    Create mitha1.py

---
+++ mitha1.py
@@ -0,0 +1,10 @@
+#NOTE:  This must be the first call in order to work properly!
+from deoldify import device
+from deoldify.device_id import DeviceId
+#choices:  CPU, GPU0...GPU7
+device.set(device=DeviceId.GPU0)
+
+import torch
+
+if not torch.cuda.is_available():
+    print('GPU not available.')
